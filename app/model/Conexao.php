bdac48bfe3459022ed18fe1e32ba7bc7fb87851f
Marcos Cordeiro <marcosc974@gmail.com>
2022-01-15 21:08:25 -0300

    Atualizando o arquivo de conexão

--- app/model/Conexao.php
+++ app/model/Conexao.php
@@ -5,11 +5,15 @@ namespace App\Model;
 class Conexao
 {
     public static $conexao;
+    public static $host = "localhost";
+    public static $dbname = "husky_db";
+    public static $username = "root";
+    public static $password = "";
 
     public static function conectar()
     {
         if (!isset(self::$conexao)) {
-            self::$conexao = new \PDO("mysql:host=localhost;dbname=husky_db;charset=utf8", "root", "");
+            self::$conexao = new \PDO("mysql:host=" . self::$host . ";dbname=" . self::$dbname . ";charset=utf8", self::$username, self::$password);
             self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
         return self::$conexao;
